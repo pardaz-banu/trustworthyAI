e7b11cad02cefcd63b644df65a30223b87631b3d
pardazbanumohammad <pardazbanu1999@gmail.com>
2025-02-19 16:52:50 -0600

    updated decoder_graph

--- research/Causal Discovery with RL/src/models/decoder/decoder_graph.py
+++ research/Causal Discovery with RL/src/models/decoder/decoder_graph.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
-from tensorflow.contrib.rnn import LSTMCell, GRUCell, MultiRNNCell, DropoutWrapper
-from tensorflow.contrib import distributions as distr
-
-from .encoder import Attentive_encoder
-
+from tensorflow.keras.layers import LSTM, GRU, Dropout, LSTMCell, GRUCell
+from tensorflow.keras import layers
 
 # RNN decoder for pointer network
 class Pointer_decoder(object):
@@ -13,60 +10,58 @@ class Pointer_decoder(object):
         ########## Reference vectors ##########
         #######################################
 
-        self.encoder_output = encoder_output # Tensor [Batch size x time steps x cell.state_size] to attend to
-        self.h = tf.transpose(self.encoder_output, [1, 0, 2]) # [Batch size x time steps x cell.state_size] to [time steps x Batch size x cell.state_size]
+        self.encoder_output = encoder_output  # Tensor [Batch size x time steps x cell.state_size] to attend to
+        self.h = tf.transpose(self.encoder_output, [1, 0, 2])  # [Batch size x time steps x cell.state_size] to [time steps x Batch size x cell.state_size]
 
         ############################
         ########## Config ##########
         ############################
 
-        batch_size = encoder_output.get_shape().as_list()[0] # batch size
-        self.seq_length = encoder_output.get_shape().as_list()[1] # sequence length
-        n_hidden = encoder_output.get_shape().as_list()[2] # num_neurons
+        batch_size = encoder_output.shape[0]  # batch size
+        self.seq_length = encoder_output.shape[1]  # sequence length
+        n_hidden = encoder_output.shape[2]  # num_neurons
 
-        self.inference_mode = config.inference_mode # True for inference, False for training
-        self.temperature = config.temperature # temperature parameter
-        self.C = config.C # logit clip
+        self.inference_mode = config.inference_mode  # True for inference, False for training
+        self.temperature = config.temperature  # temperature parameter
+        self.C = config.C  # logit clip
 
         ##########################################
         ########## Decoder's parameters ##########
         ##########################################
 
         # Variables initializer
-        initializer = tf.contrib.layers.xavier_initializer() 
+        initializer = tf.keras.initializers.GlorotUniform()  # Using Keras' Glorot initializer
 
         # Decoder LSTM cell        
-        self.cell = LSTMCell(n_hidden, initializer=initializer)
+        self.cell = LSTMCell(n_hidden, kernel_initializer=initializer)
 
         # Decoder initial input is 'GO', a variable tensor
-        first_input = tf.get_variable("GO",[1,n_hidden], initializer=initializer)
-        self.decoder_first_input = tf.tile(first_input, [batch_size,1])
+        self.decoder_first_input = self._initialize_decoder_first_input(batch_size, n_hidden, initializer)
 
         # Decoder initial state (tuple) is trainable
-        first_state = tf.get_variable("GO_state1",[1,n_hidden], initializer=initializer)
-        self.decoder_initial_state = tf.tile(first_state, [batch_size,1]), tf.reduce_mean(self.encoder_output,1)
+        self.decoder_initial_state = self._initialize_decoder_initial_state(batch_size, n_hidden, initializer)
 
         # Attending mechanism
-        with tf.variable_scope("glimpse") as glimpse:
-            self.W_ref_g =tf.get_variable("W_ref_g",[1,n_hidden,n_hidden],initializer=initializer)
-            self.W_q_g =tf.get_variable("W_q_g",[n_hidden,n_hidden],initializer=initializer)
-            self.v_g =tf.get_variable("v_g",[n_hidden],initializer=initializer)
+        with tf.variable_scope("glimpse"):
+            self.W_ref_g = self._initialize_variable([1, n_hidden, n_hidden], initializer)
+            self.W_q_g = self._initialize_variable([n_hidden, n_hidden], initializer)
+            self.v_g = self._initialize_variable([n_hidden], initializer)
 
         # Pointing mechanism
-        with tf.variable_scope("pointer") as pointer:
-            self.W_ref =tf.get_variable("W_ref",[1,n_hidden,n_hidden],initializer=initializer)
-            self.W_q =tf.get_variable("W_q",[n_hidden,n_hidden],initializer=initializer)
-            self.v =tf.get_variable("v",[n_hidden],initializer=initializer)
+        with tf.variable_scope("pointer"):
+            self.W_ref = self._initialize_variable([1, n_hidden, n_hidden], initializer)
+            self.W_q = self._initialize_variable([n_hidden, n_hidden], initializer)
+            self.v = self._initialize_variable([n_hidden], initializer)
 
         ######################################
         ########## Decoder's output ##########
         ######################################
 
-        self.log_softmax = [] # store log(p_theta(pi(t)|pi(<t),s)) for backprop
+        self.log_softmax = []  # store log(p_theta(pi(t)|pi(<t),s)) for backprop
         self.samples = []
-        self.positions = [] # store visited cities for reward
-        self.attending = [] # for vizualition
-        self.pointing = [] # for vizualition
+        self.positions = []  # store visited cities for reward
+        self.attending = []  # for visualization
+        self.pointing = []  # for visualization
 
         self.s_check = 0
         self.i_check = 0
@@ -76,53 +71,63 @@ class Pointer_decoder(object):
         ########################################
 
         # Keep track of first city
-        self.first_city_hot = 0 ###########
+        self.first_city_hot = 0
 
         # Keep track of visited cities
         self.mask = 0
         self.mask_scores = []
 
+    def _initialize_variable(self, shape, initializer):
+        return tf.Variable(initializer(shape), trainable=True)
+
+    def _initialize_decoder_first_input(self, batch_size, n_hidden, initializer):
+        first_input = tf.Variable(initializer([1, n_hidden]), trainable=True)
+        return tf.tile(first_input, [batch_size, 1])
+
+    def _initialize_decoder_initial_state(self, batch_size, n_hidden, initializer):
+        first_state = tf.Variable(initializer([1, n_hidden]), trainable=True)
+        return tf.tile(first_state, [batch_size, 1]), tf.reduce_mean(self.encoder_output, 1)
+
     # From a query (decoder output) [Batch size, n_hidden] and a set of reference (encoder_output) [Batch size, seq_length, n_hidden]
-    # predict a distribution over next decoder input
-    def attention(self,ref,query):
+    # predict a distribution over the next decoder input
+    def attention(self, ref, query):
 
         # Attending mechanism
-        encoded_ref_g = tf.nn.conv1d(ref, self.W_ref_g, 1, "VALID", name="encoded_ref_g") # [Batch size, seq_length, n_hidden]
-        encoded_query_g = tf.expand_dims(tf.matmul(query, self.W_q_g, name="encoded_query_g"), 1) # [Batch size, 1, n_hidden]
-        scores_g = tf.reduce_sum(self.v_g * tf.tanh(encoded_ref_g + encoded_query_g), [-1], name="scores_g") # [Batch size, seq_length]
+        encoded_ref_g = tf.nn.conv1d(ref, self.W_ref_g, 1, "VALID", name="encoded_ref_g")  # [Batch size, seq_length, n_hidden]
+        encoded_query_g = tf.expand_dims(tf.matmul(query, self.W_q_g, name="encoded_query_g"), 1)  # [Batch size, 1, n_hidden]
+        scores_g = tf.reduce_sum(self.v_g * tf.tanh(encoded_ref_g + encoded_query_g), [-1], name="scores_g")  # [Batch size, seq_length]
 
         # Attend to current city and cities to visit only (Apply mask)
-        attention_g = tf.nn.softmax(scores_g - 100000000.*self.mask,name="attention_g")  ###########
-        #attention_g = tf.nn.softmax(scores_g, name='attention_g') # why self.first_city? so remove for our use
+        attention_g = tf.nn.softmax(scores_g - 100000000. * self.mask, name="attention_g")  ###########
         self.attending.append(attention_g)
 
         # 1 glimpse = Linear combination of reference vectors (defines new query vector)
-        glimpse = tf.multiply(ref, tf.expand_dims(attention_g,2))
-        glimpse = tf.reduce_sum(glimpse,1)+query  ########### Residual connection
+        glimpse = tf.multiply(ref, tf.expand_dims(attention_g, 2))
+        glimpse = tf.reduce_sum(glimpse, 1) + query  ########### Residual connection
 
         # Pointing mechanism with 1 glimpse
-        encoded_ref = tf.nn.conv1d(ref, self.W_ref, 1, "VALID", name="encoded_ref") # [Batch size, seq_length, n_hidden]
-        encoded_query = tf.expand_dims(tf.matmul(glimpse, self.W_q, name="encoded_query"), 1) # [Batch size, 1, n_hidden]
-        scores = tf.reduce_sum(self.v * tf.tanh(encoded_ref + encoded_query), [-1], name="scores") # [Batch size, seq_length]
-        if self.inference_mode == True:
-            scores = scores/self.temperature # control diversity of sampling (inference mode)
-        scores = self.C*tf.tanh(scores) # control entropy
+        encoded_ref = tf.nn.conv1d(ref, self.W_ref, 1, "VALID", name="encoded_ref")  # [Batch size, seq_length, n_hidden]
+        encoded_query = tf.expand_dims(tf.matmul(glimpse, self.W_q, name="encoded_query"), 1)  # [Batch size, 1, n_hidden]
+        scores = tf.reduce_sum(self.v * tf.tanh(encoded_ref + encoded_query), [-1], name="scores")  # [Batch size, seq_length]
+        if self.inference_mode:
+            scores = scores / self.temperature  # control diversity of sampling (inference mode)
+        scores = self.C * tf.tanh(scores)  # control entropy
 
         # Point to cities to visit only (Apply mask)
-        masked_scores = scores - 100000000.*self.mask # [Batch size, seq_length]
-        pointing = tf.nn.softmax(masked_scores, name="attention") # [Batch size, Seq_length]
+        masked_scores = scores - 100000000. * self.mask  # [Batch size, seq_length]
+        pointing = tf.nn.softmax(masked_scores, name="attention")  # [Batch size, Seq_length]
         self.pointing.append(pointing)
-        
+
         return masked_scores
 
     # One pass of the decode mechanism
-    def decode(self,prev_state,prev_input,timestep):
+    def decode(self, prev_state, prev_input, timestep):
         with tf.variable_scope("loop"):
             if timestep > 0:
                 tf.get_variable_scope().reuse_variables()
 
             # Run the cell on a combination of the previous input and state
-            output, state = self.cell(prev_input,prev_state)
+            output, state = self.cell(prev_input, prev_state)
 
             # mask before masked-scores
             position = tf.ones([prev_input.shape[0]]) * timestep
@@ -135,8 +140,8 @@ class Pointer_decoder(object):
             masked_scores = self.attention(self.encoder_output, output)
 
             # we cast to Bernoulli and sample
-            prob = distr.Bernoulli(masked_scores)
-            sampled_arr = prob.sample() # Batch_size, seqlenght for just one node
+            prob = tf.distributions.Bernoulli(probs=masked_scores)
+            sampled_arr = prob.sample()  # Batch_size, seq_length for just one node
 
             self.samples.append(sampled_arr)
             self.mask_scores.append(masked_scores)
@@ -146,7 +151,7 @@ class Pointer_decoder(object):
                 self.first_city_hot = tf.one_hot(self.first_city, self.seq_length)
 
             # Retrieve decoder's new input
-            new_decoder_input = tf.gather(self.h,position)[0]
+            new_decoder_input = tf.gather(self.h, position)[0]
 
             return state, new_decoder_input
 
@@ -163,11 +168,11 @@ class Pointer_decoder(object):
         self.positions.append(self.first_city)
 
         # Stack visited indices
-        self.positions = tf.stack(self.positions, axis=1)  # [Batch,seq_length+1]
+        self.positions = tf.stack(self.positions, axis=1)  # [Batch, seq_length+1]
 
         # Stack attending & pointing distribution
-        self.attending = tf.stack(self.attending, axis=1)  # [Batch,seq_length,seq_length]
-        self.pointing = tf.stack(self.pointing, axis=1)  # [Batch,seq_length,seq_length]
+        self.attending = tf.stack(self.attending, axis=1)  # [Batch, seq_length, seq_length]
+        self.pointing = tf.stack(self.pointing, axis=1)  # [Batch, seq_length, seq_length]
 
         # Return stacked lists of visited_indices and log_softmax for backprop
         return self.samples, self.mask_scores
@@ -185,12 +190,11 @@ class Pointer_decoder(object):
         self.positions.append(self.first_city)
 
         # Stack visited indices
-        self.positions = tf.stack(self.positions, axis=1)  # [Batch,seq_length+1]
+        self.positions = tf.stack(self.positions, axis=1)  # [Batch, seq_length+1]
 
         # Stack attending & pointing distribution
-        self.attending = tf.stack(self.attending, axis=1)  # [Batch,seq_length,seq_length]
-        self.pointing = tf.stack(self.pointing, axis=1)  # [Batch,seq_length,seq_length]
+        self.attending = tf.stack(self.attending, axis=1)  # [Batch, seq_length, seq_length]
+        self.pointing = tf.stack(self.pointing, axis=1)  # [Batch, seq_length, seq_length]
 
         # Return stacked lists of visited_indices and log_softmax for backprop
         return self.samples, self.mask_scores
-
