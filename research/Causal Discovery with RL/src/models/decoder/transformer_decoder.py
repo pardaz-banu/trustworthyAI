59835947552c11fa229fddc3b97c60b2bee3ff3a
pardazbanumohammad <pardazbanu1999@gmail.com>
2025-02-19 17:16:36 -0600

    updated transformer_decoder.py

--- research/Causal Discovery with RL/src/models/decoder/transformer_decoder.py
+++ research/Causal Discovery with RL/src/models/decoder/transformer_decoder.py
@@ -65,4 +65,62 @@ class TransformerDecoder(tf.keras.Model):
         self.is_training = is_train
 
         # Initialize embedding layer and attention/ffn stacks
-        self.embedding_layer = Dense(self.input_embed, activ
+        self.embedding_layer = Dense(self.input_embed, activation=tf.nn.relu, kernel_initializer=self.initializer)
+        self.attention_blocks = [multihead_attention for _ in range(self.num_stacks)]
+        self.ffn_blocks = [feedforward for _ in range(self.num_stacks)]
+        
+    def call(self, inputs, training=True):
+        # Apply embedding and attention/ffn blocks
+        all_user_embedding = tf.reduce_mean(inputs, 1)
+        inputs_with_all_user_embedding = tf.concat([inputs, 
+                                                   tf.tile(tf.expand_dims(all_user_embedding, 1), [1, self.max_length, 1])], -1)
+
+        embedded_input = self.embedding_layer(inputs_with_all_user_embedding)
+        enc = embedded_input
+        
+        # Apply multi-head attention and feedforward blocks
+        for i in range(self.num_stacks):
+            enc = self.attention_blocks[i](enc, num_units=self.input_embed, num_heads=self.num_heads, dropout_rate=0.0, is_training=self.is_training)
+            enc = self.ffn_blocks[i](enc, num_units=[self.input_embed, self.input_embed], is_training=self.is_training)
+        
+        # Readout layer
+        adj_prob = Dense(self.max_length, activation=None)(enc)
+
+        samples = []
+        mask_scores = []
+        entropy = []
+
+        for i in range(self.max_length):
+            position = tf.ones([inputs.shape[0]]) * i
+            position = tf.cast(position, tf.int32)
+            mask = tf.one_hot(position, self.max_length)
+
+            masked_score = adj_prob[:, i, :] - 1e8 * mask
+            prob = distr.Bernoulli(logits=masked_score)
+            sampled_arr = prob.sample()
+
+            samples.append(sampled_arr)
+            mask_scores.append(masked_score)
+            entropy.append(prob.entropy())
+
+        return samples, mask_scores, entropy
+
+
+# Main execution to test the decoder
+if __name__ == '__main__':
+    config = {
+        'batch_size': 32,
+        'max_length': 10,
+        'hidden_dim': 64,
+        'num_heads': 8,
+        'num_stacks': 4
+    }
+
+    input_ = tf.random.normal([config['batch_size'], config['max_length'], config['hidden_dim']])
+    decoder = TransformerDecoder(config, is_train=True)
+    samples, mask_scores, entropy = decoder(input_)
+
+    # Print the outputs (check shapes of the returned tensors)
+    print("Samples:", [sample.shape for sample in samples])
+    print("Mask Scores:", [score.shape for score in mask_scores])
+    print("Entropy:", [e.shape for e in entropy])
