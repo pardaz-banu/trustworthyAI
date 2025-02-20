3489fb81f2d0327e66eb47412786b1876ed94861
pardazbanumohammad <pardazbanu1999@gmail.com>
2025-02-19 18:03:53 -0600

    updated

--- research/Causal Discovery with RL/src/models/critic.py
+++ research/Causal Discovery with RL/src/models/critic.py
@@ -1,14 +1,9 @@
 import tensorflow as tf
-from tensorflow.contrib.rnn import LSTMCell, MultiRNNCell, DropoutWrapper
 import numpy as np
-#from tqdm import tqdm
-
 
 class Critic(object):
- 
- 
     def __init__(self, config, is_train):
-        self.config=config
+        self.config = config
 
         # Data config
         self.batch_size = config.batch_size 
@@ -18,19 +13,23 @@ class Critic(object):
         # Network config
         self.input_embed = config.hidden_dim 
         self.num_neurons = config.hidden_dim 
-        self.initializer = tf.contrib.layers.xavier_initializer() 
+        self.initializer = tf.keras.initializers.GlorotUniform()  # Xavier initializer equivalent
 
         # Baseline setup
         self.init_baseline = 0.
- 
+
     def predict_rewards(self, encoder_output):
         # [Batch size, Sequence Length, Num_neurons] to [Batch size, Num_neurons]
-        frame = tf.reduce_mean(encoder_output, 1) 
- 
-        with tf.variable_scope("ffn"):
-            # ffn 1
-            h0 = tf.layers.dense(frame, self.num_neurons, activation=tf.nn.relu, kernel_initializer=self.initializer)
-            # ffn 2
-            w1 =tf.get_variable("w1", [self.num_neurons, 1], initializer=self.initializer)
+        frame = tf.reduce_mean(encoder_output, axis=1) 
+
+        with tf.variable_scope("ffn", reuse=tf.AUTO_REUSE):
+            # Feedforward layer 1
+            h0 = tf.keras.layers.Dense(self.num_neurons, activation=tf.nn.relu, kernel_initializer=self.initializer)(frame)
+            # Weight and bias for the output layer
+            w1 = tf.Variable(self.initializer([self.num_neurons, 1]), name="w1")
             b1 = tf.Variable(self.init_baseline, name="b1")
-            self.predictions = tf.squeeze(tf.matmul(h0, w1)+b1)
+            
+            # Compute predictions
+            self.predictions = tf.squeeze(tf.matmul(h0, w1) + b1)
+
+        return self.predictions
